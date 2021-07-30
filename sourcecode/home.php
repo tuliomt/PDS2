c614b33445a848761d4c036d26a4a71b94ceb6a8
tuliomt <tulio.oliveira@ufu.br>
2021-07-30 18:22:25 -0300

    new page
    novas paginas

---
+++ sourcecode/home.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>Pet Shop</title>
+		<link rel="stylesheet" type="text/css" href="../CSS\bootstrap.css"> 
+		 
+	
+	<script type="text/javascript" src="../JS/jquery.js"></script>
+	<script src="../JS/bootstrap.js"></script>
+
+</head>
+
+<body>
+		
+		  <div class="topnav">
+            <a>Pet Shop Cinco Estrelas</a>
+            <div class="topnav-right">
+			  <li><a href="">Sair</a></li>
+            </div>
+          </div>
+	
+
+	<div class="screen">
+		 
+	<div class="navbar-collapse collapse">
+
+				    <ul class="nav navbar-nav navbar-left">
+						<li class="active"><a href="home.php">Home</a></li>
+						<li class="dropdown ">
+						    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Consulta<b class="caret"></b></a>
+						    <ul class="dropdown-menu">
+						    </ul>
+						</li>			
+						<li class="dropdown ">
+						    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Produto<b class="caret"></b></a>
+						    <ul class="dropdown-menu">
+
+							<li class="divider"></li>
+						    </ul>
+						</li>
+						<li class="dropdown">
+						    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Animal<b class="caret"></b></a>
+						    <ul class="dropdown-menu">
+							<li class="divider"></li>
+						    </ul>
+						</li>
+
+						<li class="dropdown">
+						    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Cliente<b class="caret"></b></a>
+						    <ul class="dropdown-menu">
+						    </ul>
+						</li>
+
+						<li class="dropdown">
+						    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Funcionário<b class="caret"></b></a>
+						    <ul class="dropdown-menu">
+						    </ul>
+						</li>
+
+						<li class="dropdown">
+						    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Banho e Tosa<b class="caret"></b></a>
+						    <ul class="dropdown-menu">
+						    </ul>
+						</li>
+
+						<li class="dropdown">
+						    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Vendas<b class="caret"></b></a>
+						    <ul class="dropdown-menu">
+						    </ul>
+						</li>
+
+							<li class="dropdown">
+						    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Produtos<b class="caret"></b></a>
+						    <ul class="dropdown-menu">
+						    </ul>
+						</li>
+						
+				    </ul>
+				</div>
+	</div>	
+
+
+
+</body>
+</html>
---
+++ sourcecode/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt_BR">
+<head>
+	<title>Login</title>
+	<meta charset="UTF8"/>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="../CSS/index.css">
+	<script type="text/javascript" src="../JS/jquery.js"></script>
+	<script src="../JS/bootstrap.js"></script>
+</head>
+
+<body>
+  <div class="screen">  
+  <div class="container">
+	<form class="form-signin" method="POST">
+		<div class="col-lg-4 text-center"></div>
+		
+		<div class="col-lg-4 text-center">
+
+			<h2><p>Por favor, forneça:</p></h2>
+			
+			
+			 <div class="login-page">
+  			<div class="form">
+    		<input type="text" class="form-control" name="iemail" placeholder="Email" required="" autofocus="" contenteditable="false">
+			<p></p>
+			<input type="password" class="form-control" name="isenha" placeholder="Password" required="" contenteditable="false">
+			<label class="checkbox">
+			<button class="btn btn-lg btn-primary btn-block">Acessar</button>
+			
+			
+    		</form>
+  			</div>
+			</div>
+
+
+		</div>
+
+	 	
+	</form>
+	</div>
+</div>
+
+       
+</body>
+</html>
